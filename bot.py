3ca237cabdd787e736fd64b5768e7db60f846c3e
Anastasiia Sakhno <ask@Anastasiias-MacBook-Pro.local>
2019-03-20 10:23:36 +0200

    pooling if local, bind to ip if heroku

--- bot.py
+++ bot.py
@@ -172,11 +172,14 @@ def main():
 		dp.add_error_handler(error)
 
 		# Start the Bot
-		updater.start_polling()
-		updater.start_webhook(listen="0.0.0.0",
-		                      port=PORT,
-		                      url_path=TOKEN)
-		updater.bot.set_webhook("https://telegra-bots.herokuapp.com/" + TOKEN)
+		updater.bot.set_webhook()
+		if "HEROKU" in list(os.environ.keys()):
+			updater.start_webhook(listen="0.0.0.0",
+			                      port=PORT,
+			                      url_path=TOKEN)
+			updater.bot.set_webhook("https://telegra-bots.herokuapp.com/" + TOKEN)
+		else:
+			updater.start_polling()
 
 		# Run the bot until you press Ctrl-C or the process receives SIGINT,
 		# SIGTERM or SIGABRT. This should be used most of the time, since
