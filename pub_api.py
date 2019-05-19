c7dda60cfac400ac5d290ebb728305e5d04017e5
Anastasiia Sakhno <ask@Anastasiias-MacBook-Pro.local>
2019-05-19 10:56:51 +0300

    pub api exceptions handling draft

--- pub_api.py
+++ pub_api.py
@@ -17,123 +17,153 @@ AVAILABLE_TO_TIMES_URL = f'{BASE_API_URL}/table_reservations/available_to_times'
 BLACK_LIST_URL = f'{BASE_API_URL}/table_reservations/black_lists'
 
 def auth_token():
-	payload = {
-		'user': {
-			'email': os.environ.get('ADMIN_EMAIL'),
-			'password': os.environ.get('ADMIN_PASSWORD')
-		}
-  }
+  try:
+  	payload = {
+  		'user': {
+  			'email': os.environ.get('ADMIN_EMAIL'),
+  			'password': os.environ.get('ADMIN_PASSWORD')
+  		}
+    }
 
-	headers = {
-		'Content-type': 'application/json'
-	} 
+  	headers = {
+  		'Content-type': 'application/json'
+  	} 
 
-	response = requests.post(LOGIN_URL, data = json.dumps(payload), headers = headers)
+  	response = requests.post(LOGIN_URL, data = json.dumps(payload), headers = headers)
 
-	return response.json()['token']
+  	return response.json()['token']
+  except:
+    return None
 
 
 def post_table_reservation(chat_id, date):
-  payload = {
-    'table_reservation': {
-      'chat_id': chat_id,
-      'date': date
+  try:
+    payload = {
+      'table_reservation': {
+        'chat_id': chat_id,
+        'date': date
+      }
     }
-  }
 
-  headers = {
-    'Content-type': 'application/json',
-    'Authorization': auth_token()
-  } 
+    headers = {
+      'Content-type': 'application/json',
+      'Authorization': auth_token()
+    } 
 
-  response = requests.post(TABLE_RESERVATIONS_URL, data = json.dumps(payload), headers = headers)
+    response = requests.post(TABLE_RESERVATIONS_URL, data = json.dumps(payload), headers = headers)
 
-  return response.json()
+    return response.json()
+  except:
+    return None
 
 
 def get_blackout_dates(date, hall):
-  headers = {
-    'Authorization': auth_token()
-  } 
+  try:
+    headers = {
+      'Authorization': auth_token()
+    } 
 
-  response = requests.get(f'{BLACKOUT_DATES_URL}?date={date}&hall={hall}', headers = headers)
+    response = requests.get(f'{BLACKOUT_DATES_URL}?date={date}&hall={hall}', headers = headers)
 
-  return response.json()
+    return response.json()
+  except:
+    return None
 
 
 def get_black_list(user_phone):
-  headers = {
-    'Authorization': auth_token()
-  } 
+  try:
+    headers = {
+      'Authorization': auth_token()
+    } 
 
-  response = requests.get(f'{BLACK_LIST_URL}?user_phone={user_phone}', headers = headers)
+    response = requests.get(f'{BLACK_LIST_URL}?user_phone={user_phone}', headers = headers)
 
-  return response.json()
+    return response.json()
+  except:
+    return None
 
 
 def get_latest_table_reservation(chat_id):
-  headers = {
-    'Authorization': auth_token()
-  } 
+  try:
+    headers = {
+      'Authorization': auth_token()
+    } 
 
-  response = requests.get(f'{LATEST_TABLE_RESERVATION_URL}?chat_id={chat_id}', headers = headers)
+    response = requests.get(f'{LATEST_TABLE_RESERVATION_URL}?chat_id={chat_id}', headers = headers)
 
-  return response.json()
+    return response.json()
+  except:
+    return None
 
 
 def put_table_reservation(chat_id, attr, value):
-  item = get_latest_table_reservation(chat_id)
+  try:
+    item = get_latest_table_reservation(chat_id)
 
-  payload = {
-    'table_reservation': {
-      f'{attr}': value
+    payload = {
+      'table_reservation': {
+        f'{attr}': value
+      }
     }
-  }
 
-  headers = {
-    'Content-type': 'application/json',
-    'Authorization': auth_token()
-  } 
+    headers = {
+      'Content-type': 'application/json',
+      'Authorization': auth_token()
+    } 
 
-  response = requests.put(f'{TABLE_RESERVATIONS_URL}/{item["id"]}', data = json.dumps(payload), headers = headers)
+    response = requests.put(f'{TABLE_RESERVATIONS_URL}/{item["id"]}', data = json.dumps(payload), headers = headers)
+  except:
+    return None
 
 
 def delete_table_reservation(chat_id):
-  item = get_latest_table_reservation(chat_id)
+  try:
+    item = get_latest_table_reservation(chat_id)
 
-  headers = {
-    'Content-type': 'application/json',
-    'Authorization': auth_token()
-  } 
+    headers = {
+      'Content-type': 'application/json',
+      'Authorization': auth_token()
+    } 
 
-  response = requests.delete(f'{TABLE_RESERVATIONS_URL}/{item["id"]}', headers = headers)
+    response = requests.delete(f'{TABLE_RESERVATIONS_URL}/{item["id"]}', headers = headers)
+  except:
+    return None
 
 
 def get_available_tables(chat_id):
-  headers = {
-    'Authorization': auth_token()
-  } 
+  try:
+    headers = {
+      'Authorization': auth_token()
+    } 
 
-  response = requests.get(f'{AVAILABLE_TABLES_URL}?chat_id={chat_id}', headers = headers)
+    response = requests.get(f'{AVAILABLE_TABLES_URL}?chat_id={chat_id}', headers = headers)
 
-  return response.json()
+    return response.json()
+  except:
+    return None
 
 
 def get_available_from_times():
-  headers = {
-    'Authorization': auth_token()
-  } 
+  try:
+    headers = {
+      'Authorization': auth_token()
+    } 
 
-  response = requests.get(AVAILABLE_FROM_TIMES_URL, headers = headers)
+    response = requests.get(AVAILABLE_FROM_TIMES_URL, headers = headers)
 
-  return response.json()
+    return response.json()
+  except:
+    return None
 
 
 def get_available_to_times(chat_id):
-  headers = {
-    'Authorization': auth_token()
-  } 
+  try:
+    headers = {
+      'Authorization': auth_token()
+    } 
 
-  response = requests.get(f'{AVAILABLE_TO_TIMES_URL}?chat_id={chat_id}', headers = headers)
+    response = requests.get(f'{AVAILABLE_TO_TIMES_URL}?chat_id={chat_id}', headers = headers)
 
-  return response.json()
+    return response.json()
+  except:
+    return None
